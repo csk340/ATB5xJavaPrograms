052907b490780a942bb0f72b52c096a68d838c6c
ChinmayRepo <chinmayk340@gmail.com>
2024-01-01 21:06:35 +0530

    Commit and push is done

---
+++ PracticeMakesManPerfect/FizzBuzz.java
@@ -0,0 +1,27 @@
+package PracticeMakesManPerfect;
+/*Problem statement:-
+1. FizzBuzz Problem: Write a program that prints numbers from 1 to 100.
+For multiples of 3, print "Fizz" instead of the number,
+and for the multiples of 5, print "Buzz".
+For numbers which are multiples of both 3 and 5, print "FizzBuzz"
+ */
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i=1;i<=100;i++)
+        {
+            if (i%3 == 0 && i%5 ==0)
+            {
+                System.out.println(i+" : FizzBuzz");
+            }
+            else if (i%3==0)
+            {
+                System.out.println(i+" : Fizz");
+            }
+            else if (i%5==0)
+            {
+                System.out.println(i+" : Buzz");
+            }
+        }
+
+    }
+}
