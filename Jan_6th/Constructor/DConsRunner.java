239e8b5d53387b6663543609ee898036bdc7e59d
ChinmayRepo <chinmayk340@gmail.com>
2024-01-21 23:38:34 +0530

    Commit and push is done

---
+++ Jan_6th/Constructor/DConsRunner.java
@@ -0,0 +1,9 @@
+package Jan_6th.Constructor;
+
+public class DConsRunner {
+    public static void main(String[] args) {
+        DemoConstructor obj = new DemoConstructor();//Default Constructor
+        DemoConstructor obj2 = new DemoConstructor(111);//Parameterized Constructor
+        DemoConstructor obj3 = new DemoConstructor("chinmay", 124,80.67 ,  'A');
+    }
+}
