12ad415e7a2a39456ebcf7f358815a56d8c7e644
ChinmayRepo <chinmayk340@gmail.com>
2024-01-30 23:43:59 +0530

    Commit and push is done

---
+++ Jan_7th_OOPS/Inheritance/Multi_Level_Inheritance/GrandFather.java
@@ -0,0 +1,8 @@
+package Jan_7th_OOPS.Inheritance.Multi_Level_Inheritance;
+
+public class GrandFather {
+    public void fightBehavior()
+    {
+        System.out.println("Grandfather: I love battle");
+    }
+}
