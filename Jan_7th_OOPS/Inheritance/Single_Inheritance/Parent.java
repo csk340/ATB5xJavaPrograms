b089fa5cc5ae5af48f76395cc3988976b361c8a0
ChinmayRepo <chinmayk340@gmail.com>
2024-01-30 23:44:14 +0530

    Commit and push is done

---
+++ Jan_7th_OOPS/Inheritance/Single_Inheritance/Parent.java
@@ -0,0 +1,14 @@
+package Jan_7th_OOPS.Inheritance.Single_Inheritance;
+
+public class Parent {
+
+    public void speak()
+    {
+        System.out.println("I am from parent speak");
+    }
+
+    public void walk()
+    {
+        System.out.println("I am from parent XYZ style walk");
+    }
+}
